8c60d9b7d452601e8bdfa94c63c88bb1f50c9d5c
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-02 00:04:10 -0300

    refactor: refactor session model class with inheritance of base model

--- model/Session.py
+++ model/Session.py
@@ -1,23 +1,14 @@
 from datetime import datetime
+from pydantic import BaseModel
 
 
-class Session:
-    def __init__(self,
-                 movie_id: int,
-                 room_id: int,
-                 start_time: datetime,
-                 end_time: datetime,
-                 price: float,
-                 language: str,
-                 subtitles: bool,
-                 create_at: datetime,
-                 update_at: datetime):
-        self.movie_id = movie_id
-        self.room_id = room_id
-        self.start_time = start_time
-        self.end_time = end_time
-        self.price = price
-        self.language = language
-        self.subtitles = subtitles
-        self.create_at = create_at
-        self.update_at = update_at
+class Session(BaseModel):
+     movie_id: int
+     room_id: int
+     start_time: datetime
+     end_time: datetime
+     price: float
+     language: str
+     subtitles: bool
+     create_at: datetime
+     update_at: datetime

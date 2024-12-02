cb7f732d8dc8e771a815a72c79cf21bff9b14202
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-02 00:05:38 -0300

    feat: create admin and customer service

---
+++ service/AdminService.py
@@ -0,0 +1,47 @@
+from fastapi import HTTPException, FastAPI
+from fastapi.openapi.models import Response
+
+from model.Admin import Admin
+from model.AuthModel import AuthModel
+from repository.AdminRepository import AdminRepository
+
+app = FastAPI()
+
+class AdminService:
+    def __init__(self, admin_repository: AdminRepository):
+        self.admin_repository = admin_repository
+
+    @app.post("/admin", status_code=201)
+    async def create(self, admin: Admin):
+        response = self.admin_repository.save(admin)
+        if response:
+            return {"message": "Admin created"}
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    @app.get("/admin/{id}")
+    async def get_by_id(self, id: int):
+        response = self.admin_repository.get_by_id(id)
+        if response:
+            return {"admin": response}
+        raise HTTPException(status_code=404, detail="Admin not found")
+
+    @app.put("/admin/{id}")
+    async def update(self, id: int, admin: Admin):
+        response = self.admin_repository.update(id, admin)
+        if response:
+            return {"message": "Admin updated"}
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    @app.delete("/admin/{id}")
+    async def delete(self, id: int):
+        response = self.admin_repository.delete(id)
+        if response:
+            return Response(status_code=204)
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    @app.post("/admin/auth")
+    async def auth(self, auth_model: AuthModel):
+        response = self.admin_repository.authenticate(auth_model.email, auth_model.password)
+        if response:
+            return {"message": "Authentication successful", "admin": response}
+        raise HTTPException(status_code=401, detail="Authentication failed")
---
+++ service/CustomerService.py
@@ -0,0 +1,47 @@
+from fastapi.openapi.models import Response
+
+from model.AuthModel import AuthModel
+from model.Customer import Customer
+from repository.CustomerRepository import CustomerRepository
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+class CustomerService:
+    def __init__(self, customer_repository: CustomerRepository):
+        self.customer_repository = customer_repository
+
+    @app.post("/customer", status_code=201)
+    async def create(self, customer: Customer):
+        response = self.customer_repository.save(customer)
+        if response:
+            return {"message": "Customer created"}
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    @app.get("/customer/{id}")
+    async def get_by_id(self, id: int):
+        response = self.customer_repository.get_by_id(id)
+        if response:
+            return {"customer": response}
+        raise HTTPException(status_code=404, detail="Customer not found")
+
+    @app.put("/customer/{id}")
+    async def update(self, id: int, customer: Customer):
+        response = self.customer_repository.update(id, customer)
+        if response:
+            return {"message": "Customer updated"}
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    @app.delete("/customer/{id}")
+    async def delete(self, id: int):
+        response = self.customer_repository.delete(id)
+        if response:
+            return Response(status_code=204)
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    @app.post("/customer/auth")
+    async def auth(self, auth_model: AuthModel):
+        response = self.customer_repository.authenticate(auth_model.email, auth_model.password)
+        if response:
+            return {"message": "Authentication successful", "customer": response}
+        raise HTTPException(status_code=401, detail="Authentication failed")

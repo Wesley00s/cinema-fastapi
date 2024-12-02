8e9ed5878a9eacb0f9f128b46bb310c0a2f50075
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-02 00:02:03 -0300

    refactor: change customer class model inheritance

--- model/Customer.py
+++ model/Customer.py
@@ -1,16 +1,7 @@
+from model.Role import Role
 from model.User import User
 
 
 class Customer(User):
-    def __init__(self,
-                 email: str,
-                 id: int,
-                 first_name: str,
-                 last_name: str,
-                 address: str,
-                 city: str,
-                 state: str,
-                 zip: str,
-                 country: str):
-        super().__init__(id, first_name, last_name, address, city, state, zip, country)
-        self.email = email
+    age: int | None = None
+    role: Role = Role.CUSTOMER

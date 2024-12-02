9e821e16084b143709ffb7723eac1d75a26b5493
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-02 00:03:50 -0300

    refactor: refactor movie model class with inheritance of base model

--- model/Movie.py
+++ model/Movie.py
@@ -1,27 +1,14 @@
 from datetime import datetime
+from pydantic import BaseModel
 
-
-class Movie:
-    def __init__(self,
-                 id: int,
-                 title: str,
-                 genre: str,
-                 synopsis: str,
-                 duration: int,
-                 age_rating: str,
-                 director: str,
-                 cast: list,
-                 release_date: str,
-                 create_at: datetime,
-                 update_at: datetime):
-        self.id = id
-        self.title = title
-        self.genre = genre
-        self.synopsis = synopsis
-        self.duration = duration
-        self.age_rating = age_rating
-        self.director = director
-        self.cast = cast
-        self.release_date = release_date
-        self.create_at = create_at
-        self.update_at = update_at
+class Movie(BaseModel):
+    id: int
+    title: str
+    genre: str
+    synopsis: str
+    duration: int
+    age_rating: str
+    director: str
+    release_date: str
+    create_at: datetime
+    update_at: datetime

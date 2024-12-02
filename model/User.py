87e67d20368c88a06f89d373789e301b64a9a992
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-02 00:04:33 -0300

    refactor: refactor user model class with inheritance of base model

--- model/User.py
+++ model/User.py
@@ -1,18 +1,20 @@
-class User:
-    def __init__(self,
-                 id: int,
-                 first_name: str,
-                 last_name: str,
-                 address: str,
-                 city: str,
-                 state: str,
-                 zip: str,
-                 country: str):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip = zip
-        self.country = country
+from pydantic import BaseModel
+
+from model.Role import Role
+
+
+class User(BaseModel):
+    email: str | None = None
+    password: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    address: str | None = None
+    city: str | None = None
+    state: str | None = None
+    zip_code: str | None = None
+    country: str | None = None
+    role: Role | None = None
+
+    model_config = {
+        "arbitrary_types_allowed": True
+    }

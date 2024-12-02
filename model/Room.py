959d49777d34af1dd6defaab2c29cdf2ab7e520a
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-02 00:04:01 -0300

    refactor: refactor room model class with inheritance of base model

--- model/Room.py
+++ model/Room.py
@@ -1,15 +1,10 @@
 from datetime import datetime
+from pydantic import BaseModel
 
 
-class Room:
-    def __init__(self,
-                 id: int,
-                 number: int,
-                 capacity: int,
-                 created_at: datetime,
-                 updated_at: datetime):
-        self.id = id
-        self.number = number
-        self.capacity = capacity
-        self.created_at = created_at
-        self.updated_at = updated_at
+class Room(BaseModel):
+     id: int
+     number: int
+     capacity: int
+     created_at: datetime
+     updated_at: datetime

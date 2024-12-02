1f4ebc88eb7a8ac39a8f4ef272d6c51dd20d1012
Wesley00s <wesley300rodrigues@gmail.com>
2024-12-02 00:01:49 -0300

    refactor: change admin class model inheritance

--- model/Admin.py
+++ model/Admin.py
@@ -1,18 +1,6 @@
+from model.Role import Role
 from model.User import User
 
 
 class Admin(User):
-    def __init__(self,
-                 email: str,
-                 password: str,
-                 id: int,
-                 first_name: str,
-                 last_name: str,
-                 address: str,
-                 city: str,
-                 state: str,
-                 zip: str,
-                 country: str):
-        super().__init__(id, first_name, last_name, address, city, state, zip, country)
-        self.email = email
-        self.password = password
+    role: Role = Role.ADMIN

6d25c22042474a939e1772454a03da482f1afc49
Wesley00s <wesley300rodrigues@gmail.com>
2025-01-18 23:43:12 -0300

    feat: add admin login js file

---
+++ app/static/js/login-admin.js
@@ -0,0 +1,26 @@
+document.getElementById('login-form').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    try {
+        const response = await fetch('/admin/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+            await response.json();
+            window.location.href = '/home-admin';
+        } else {
+            const error = await response.json();
+            console.log('Erro: ' + error.detail);
+        }
+    } catch (e) {
+        console.log('Ocorreu um erro inesperado: ' + e.message);
+    }
+});

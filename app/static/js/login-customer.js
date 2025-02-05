d5d96b5163d98f6a7b6618da55a41201598b1204
Wesley00s <wesley300rodrigues@gmail.com>
2025-02-05 19:10:18 -0300

    update: update login customer js file

--- app/static/js/login-customer.js
+++ app/static/js/login-customer.js
@@ -2,7 +2,11 @@ document.getElementById('login-form').addEventListener('submit', async function 
     event.preventDefault();
 
     const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData.entries());
+    const data = {
+        email: formData.get('email'),
+        password: formData.get('password')
+    };
+    localStorage.setItem('email', data.email);
 
     try {
         const response = await fetch('/customer/auth', {
@@ -13,21 +17,22 @@ document.getElementById('login-form').addEventListener('submit', async function 
             body: JSON.stringify(data),
         });
 
-        if (response.ok) {
-            const userResponse = await fetch(`/customer/email?email=${data.email}`);
-            const userData = await userResponse.json();
-            sessionStorage.setItem('userId', userData.customer.id);
+        const responseData = await response.json();
+        console.log(responseData);
 
-            await response.json();
+        if (response.ok) {
+            localStorage.setItem('access_token', responseData.access_token);
             window.location.href = '/home-customer';
 
         } else {
-            const error = await response.json();
-            console.log('Erro: ' + error.detail);
-            alert('Invalid login credentials or other error occurred.');
-
+            if (response.status === 401) {
+                alert('Credenciais inválidas');
+            } else {
+                alert(`Erro: ${responseData.detail || 'Erro desconhecido'}`);
+            }
         }
     } catch (e) {
-        console.log('Ocorreu um erro inesperado: ' + e.message);
+        console.error('Erro na requisição:', e);
+        alert('Erro de conexão com o servidor');
     }
 });

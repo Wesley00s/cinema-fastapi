187318963161966865472faa18c67b65bef027e1
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-16 20:21:05 -0300

    feat: include seat router in init routers

--- app/routes/init_routers.py
+++ app/routes/init_routers.py
@@ -4,6 +4,7 @@ from .admin import router as admin_router
 from .customer import router as customer_router
 from .movie import router as movie_router
 from .room import router as room_router
+from .seat import router as seat_router
 from .session import router as session_router
 from .ticket import router as ticket_router
 
@@ -14,4 +15,5 @@ def init_routers(app: FastAPI):
     app.include_router(movie_router, tags=['Movie'])
     app.include_router(session_router, tags=['Session'])
     app.include_router(room_router, tags=['Room'])
+    app.include_router(seat_router, tags=['Seat'])
     app.include_router(ticket_router, tags=['Ticket'])

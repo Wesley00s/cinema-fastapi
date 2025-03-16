0d4c7b3f1a9900a346e859a259af8373ff7fe2de
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-16 11:35:20 -0300

    refactor: create init models

---
+++ app/database/init_models.py
@@ -0,0 +1,20 @@
+from app.database.database import engine
+from app.models import (
+    user_model,
+    admin_model,
+    customer_model,
+    movie_model,
+    room_model,
+    session_model,
+    ticket_model
+)
+
+
+def init_database_models():
+    user_model.Base.metadata.create_all(bind=engine)
+    admin_model.UserModel.metadata.create_all(bind=engine)
+    customer_model.UserModel.metadata.create_all(bind=engine)
+    movie_model.Base.metadata.create_all(bind=engine)
+    room_model.Base.metadata.create_all(bind=engine)
+    session_model.Base.metadata.create_all(bind=engine)
+    ticket_model.Base.metadata.create_all(bind=engine)

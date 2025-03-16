913868b75b6147c14103e5384bfd288b65084a3e
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-16 20:54:15 -0300

    refactor: refactor movie model and add genre enum

--- app/models/movie_model.py
+++ app/models/movie_model.py
@@ -1,17 +1,18 @@
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, Enum
 
 from app.database.database import Base
+from app.models.MovieGenre import MovieGenre
 
 
 class MovieModel(Base):
     __tablename__ = 'movie'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    title = Column(String, nullable=False)
-    genre = Column(String, nullable=False)
-    synopsis = Column(String, nullable=False)
-    duration = Column(String, nullable=False)
+    title = Column(String(100), nullable=False)
+    genre = Column(Enum(MovieGenre), nullable=False)
+    synopsis = Column(String(500), nullable=False)
+    duration = Column(Integer, nullable=False)
     age_rating = Column(Integer, nullable=False)
-    director = Column(String, nullable=False)
+    director = Column(String(100), nullable=False)
     release_date = Column(DateTime, nullable=False)
     create_at = Column(DateTime, nullable=False)
     update_at = Column(DateTime, nullable=False)

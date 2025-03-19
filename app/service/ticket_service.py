9b87bc7ed6a7135d2efc31764c0a0d2b46894210
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-19 10:58:10 -0300

    refactor: add age customer validation

--- app/service/ticket_service.py
+++ app/service/ticket_service.py
@@ -3,14 +3,29 @@ from typing import List
 
 from fastapi import HTTPException, status
 
+from app.models.customer_model import CustomerModel
+from app.models.movie_model import MovieModel
+from app.models.session_model import SessionModel
 from app.models.ticket_model import TicketModel
+from app.repositories.customer_repository import CustomerRepository
+from app.repositories.movie_repository import MovieRepository
+from app.repositories.session_repository import SessionRepository
 from app.repositories.ticket_repository import TicketRepository
 from app.schemas.ticket_schema import TicketBaseSchema
 
 
 class TicketService:
-    def __init__(self, repository: TicketRepository):
+    def __init__(
+            self,
+            repository: TicketRepository,
+            customer_repository: CustomerRepository,
+            movie_repository: MovieRepository,
+            session_repository: SessionRepository
+    ):
         self.repository = repository
+        self.customer_repository = customer_repository
+        self.movie_repository = movie_repository
+        self.session_repository = session_repository
 
     def get_all_tickets(self) -> List[TicketModel]:
         return self.repository.get_all()
@@ -25,6 +40,16 @@ class TicketService:
         return ticket
 
     def create_ticket(self, ticket_data: TicketBaseSchema) -> TicketModel:
+        customer: CustomerModel = self.customer_repository.get_by_id(ticket_data.customer_id)
+        session: SessionModel = self.session_repository.get_by_id(ticket_data.session_id)
+        movie: MovieModel = self.movie_repository.get_by_id(session.movie_id)
+
+        if customer.age < movie.age_rating:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Customer age is less than movie age rating."
+            )
+
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         ticket_dict = ticket_data.model_dump()
         ticket_dict.update({

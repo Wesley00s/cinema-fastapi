405d84e1b4cad653113490260d1d334b01f2f649
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-27 09:43:59 -0300

    feat: add room dependency in seat dependency

--- app/dependencies/seat_dependencies.py
+++ app/dependencies/seat_dependencies.py
@@ -2,6 +2,7 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
+from app.dependencies.room_dependencies import get_room_repository
 from app.repositories.seat_repository import SeatRepository
 from app.service.seat_service import SeatService
 
@@ -10,5 +11,8 @@ def get_seat_repository(db: Session = Depends(get_db)):
     return SeatRepository(db)
 
 
-def get_seat_service(repository: SeatRepository = Depends(get_seat_repository)):
-    return SeatService(repository)
+def get_seat_service(
+        repository: SeatRepository = Depends(get_seat_repository),
+        room_repository: Depends = Depends(get_room_repository)
+):
+    return SeatService(repository, room_repository)

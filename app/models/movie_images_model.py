2db3db3972aa1a88bc75c87e0475a13695d87f84
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-27 09:45:36 -0300

    refactor: add cascade constraint and relationship between movie and movie images

--- app/models/movie_images_model.py
+++ app/models/movie_images_model.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, ForeignKey, LargeBinary
+from sqlalchemy.orm import relationship
 
 from app.database.database import Base
 
@@ -6,6 +7,8 @@ from app.database.database import Base
 class MovieImagesModel(Base):
     __tablename__ = 'movie_images'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    movie_id = Column(Integer, ForeignKey('movie.id'), nullable=False)
+    movie_id = Column(Integer, ForeignKey('movie.id', ondelete='CASCADE'), nullable=False)
     poster_image_data = Column(LargeBinary, nullable=False)
     backdrop_image_data = Column(LargeBinary, nullable=False)
+
+    movie = relationship("MovieModel", back_populates="images")

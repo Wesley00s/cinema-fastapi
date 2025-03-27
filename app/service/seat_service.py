d1df5147147b588b94e88c094a9796dd5b856ba0
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-27 09:51:52 -0300

    refactor: include room dependency injection in seat service and add get seats by room number method

--- app/service/seat_service.py
+++ app/service/seat_service.py
@@ -3,19 +3,21 @@ from typing import List
 from fastapi import HTTPException, status
 
 from app.models.seat_model import SeatModel
+from app.repositories.room_repository import RoomRepository
 from app.repositories.seat_repository import SeatRepository
 from app.schemas.seat_schema import SeatBaseSchema
 
 
 class SeatService:
-    def __init__(self, repository: SeatRepository):
+    def __init__(self, repository: SeatRepository, room_repository: RoomRepository):
         self.repository = repository
+        self.room_repository = room_repository
 
     def get_all_seats(self) -> List[SeatModel]:
         return self.repository.get_all()
 
-    def get_seat_by_id(self, seat_id: int) -> SeatModel:
-        seat = self.repository.get_by_id(seat_id)
+    def get_seat_by_number(self, seat_id: int) -> SeatModel:
+        seat = self.repository.get_by_number(seat_id)
         if not seat:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
@@ -32,22 +34,31 @@ class SeatService:
             )
         return seats
 
+    def get_seats_by_room_number(self, room_number: int) -> List[SeatModel]:
+        seats = self.repository.get_by_room_number(room_number)
+        if not seats:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Room with number {room_number} not found"
+            )
+        return seats
 
-    def update_seat(self, seat_id: int, seat_data: SeatBaseSchema) -> SeatModel:
-        seat = self.repository.get_by_id(seat_id)
+
+    def update_seat(self, seat_number: int, seat_data: SeatBaseSchema) -> SeatModel:
+        seat = self.repository.get_by_number(seat_number)
         if not seat:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Seat with id {seat_id} not found"
+                detail=f"Seat with id {seat_number} not found"
             )
         update_data = seat_data.model_dump(exclude_unset=True)
-        return self.repository.update(seat_id, update_data)
+        return self.repository.update(seat_number, update_data)
 
-    def delete_seat(self, seat_id: int):
-        seat = self.repository.get_by_id(seat_id)
+    def delete_seat(self, seat_number: int):
+        seat = self.repository.get_by_number(seat_number)
         if not seat:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Seat with id {seat_id} not found"
+                detail=f"Seat with number {seat_number} not found"
             )
         self.repository.delete(seat)
